4afa394fae04c3569a0b741b60421095f3d8535e
Adli I. Ifkar <adly.shadowbane@gmail.com>
2023-02-13 21:52:48 +0700

    Add several method to rediscache
    - Set, to set cache value
    - Get, to get the cache value by key
    - Has, check if cache has key

--- cache.go
+++ cache.go
@@ -2,8 +2,10 @@ package rediscache
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/redis/go-redis/v9"
 	"go.uber.org/zap"
+	"time"
 
 	_ "github.com/shadowbane/go-logger"
 )
@@ -12,6 +14,12 @@ type RedisCache struct {
 	Connection *redis.Client
 }
 
+// Connect to Redis Server
+//
+//  Parameters:
+//   - host: Redis Server Host
+//   - password: Redis Server Password
+//   - database: Redis Server Database int
 func connect(host string, password string, database int) *redis.Client {
 	client := redis.NewClient(&redis.Options{
 		Addr:     host,
@@ -35,4 +43,79 @@ func Init(c *RedisConfig) *RedisCache {
 	return &RedisCache{
 		Connection: redisClient,
 	}
+}
+
+func (rc *RedisCache) Set(key string, value interface{}, expiration int) error {
+
+	exp := time.Duration(expiration) * time.Second
+
+	valueToStore := toJson(value)
+
+	set := rc.Connection.Set(context.Background(), key, valueToStore, exp)
+	if set.Err() != nil {
+		return set.Err()
+	}
+
+	return nil
+}
+
+func (rc *RedisCache) Get(key string) (interface{}, error) {
+	operation := rc.Connection.Get(context.Background(), key)
+
+	if operation.Err() != nil {
+		return nil, operation.Err()
+	}
+
+	result, err := operation.Result()
+	if err != nil {
+		return nil, err
+	}
+
+	if IsJson(result) {
+		return toInterface(result), nil
+	}
+
+	return result, nil
+}
+
+// Has checks if the key exists in the cache
+func (rc *RedisCache) Has(key string) bool {
+	operation := rc.Connection.Exists(context.Background(), key)
+
+	if operation.Err() != nil {
+		return false
+	}
+
+	result, err := operation.Result()
+	if err != nil {
+		return false
+	}
+
+	return result > 0
+}
+
+func IsJson(str string) bool {
+	var js json.RawMessage
+	return json.Unmarshal([]byte(str), &js) == nil
+}
+
+func toJson(value interface{}) string {
+	jsonString, err := json.Marshal(value)
+
+	if err != nil {
+		zap.S().Errorf("Error converting interface to string: %s", err.Error())
+	}
+
+	return string(jsonString)
+}
+
+func toInterface(value string) interface{} {
+	var result interface{}
+	err := json.Unmarshal([]byte(value), &result)
+
+	if err != nil {
+		zap.S().Errorf("Error converting string to interface: %s", err.Error())
+	}
+
+	return result
 }
